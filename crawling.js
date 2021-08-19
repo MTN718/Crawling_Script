107ac73bd2d84d2095b3b59f0aa8785805758597
Kons <paulmars718@outlook.com>
2021-08-19 22:18:24 +0300

    Update script

--- crawling.js
+++ crawling.js
@@ -4,35 +4,34 @@ var app = express();
 const puppeteer = require('puppeteer');
 
 process.on('message', (message) => {
-    console.log('child_process:' + message);
     if (message.cmd == 'start') {
-        console.log('Child process received START message');
-        // let slowResult = slowFunction();
-        let message = `{"totalCount":22}`;
-        process.send(message);
+        console.log('------------------Starting-----------------');
+        crawlPage(message.url,message.site_id);        
     }
 });
 
+async function crawlPage(link,site_id) {
+    try {
+        const browser = await puppeteer.launch({
+          headless: true
+        });
+        const page = await browser.newPage();
+        await page.setDefaultNavigationTimeout(0);           
+        await page.goto(link);
 
-// (async () => {
-//   try {
-//     const browser = await puppeteer.launch({
-//       headless: true
-//     });
-//     const page = await browser.newPage();
-//     await page.setDefaultNavigationTimeout(0);           
-//     await page.goto('https://www.target.com');
-
-//     let body = await page.evaluate(() => {
-//       return document.querySelector('body').innerHTML;
-//     });
-//     var hrefSegs = body.split(/href="(.*?)"/g);
-//     for (var i = 1;i < hrefSegs.length;i+=2)
-//     {
-//         console.log(hrefSegs[i] + "\n");
-//     }
-//     await browser.close();
-//   } catch (error) {
-//     console.log(error);
-//   }
-// })();
+        let body = await page.evaluate(() => {
+          return document.querySelector('body').innerHTML;
+        });
+        var hrefSegs = body.split(/href="(.*?)"/g);
+        for (var i = 1;i < hrefSegs.length;i+=2)
+        {
+            console.log(hrefSegs[i] + "\n");
+            if (hrefSegs[i].startsWith('http')) continue;
+            let message = {url:hrefSegs[i],site_id:site_id}
+            process.send(message);
+        }
+        await browser.close();
+      } catch (error) {
+        console.log(error);
+    }
+}
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var mysql = require('mysql');
 var http = require('http');
 const dotenv = require('dotenv');
+const util = require('util');
 dotenv.config();
 const { fork } = require('child_process');
 const child = fork(__dirname + '/crawling.js');
@@ -13,13 +14,56 @@ console.log(process.env.DB_PW);
 var con = mysql.createConnection({
   host: process.env.DB_SERVER,
   user: process.env.DB_USER,
-  password: process.env.DB_PW
+  password: process.env.DB_PW,
+  database: process.env.DB_NAME
 });
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
-//
+const query = util.promisify(con.query).bind(con);
+var isRunning = true;
+
+(async () => {
+  try {
+    while(isRunning)
+    {
+      const rows = await query('select * from queue where is_processing=0');
+      if (rows.length == 0)
+      {
+        isRunning = false;
+        console.log("No URL");
+      }
+      else
+      {
+        item = rows[0];
+        child.on('message', (message) => {
+          var link = message.url;
+          var site_id = message.site_id;
+
+          const siteDatas = await query('select * from sites where id=' + site_id);
+          if (siteDatas.length == 0) 
+          {
+              console.log('Unregistered Site ID');
+              return;
+          }
+          let siteData = siteDatas[0];
+          if (link.startsWith(siteData.product_url_format))
+          {
+            var sql = "INSERT INTO product_url (site_id, link) VALUES ('Company Inc', 'Highway 37')";
+            //Add Product URL
+          }
+          else
+          {
+            //Add Queue
+          }      
+        });
+        console.log(item.link);     
+        child.send({cmd:'start',url:item.link,site_id:item.site_id});      
+        await query('update queue set is_processing=1 where id=' + item.id);
+      }      
+    }
+  } finally {
+    conn.end();
+  }
+})()
+
 // child.on('message', (message) => {
 //   console.log('Returning /total results');
 //   console.log(message);
